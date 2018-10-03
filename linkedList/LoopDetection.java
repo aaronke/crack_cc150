5e01b0b16da03c9592e56ec3f022a1bad7381c2a
Ke, Aaron <aaron.ke@sap.com>
2018-10-03 09:44:33 -0700

    find the start of a loop in linked list

---
+++ linkedList/LoopDetection.java
@@ -0,0 +1,25 @@
+// Loop Detection: Given a circular linked list, implement an algorithm that returns the node at the
+// beginning of the loop.
+// DEFINITION
+// Circular linked list: A (corrupt) linked list in which a node's next pointer points to an earlier node, so
+// as to make a loop in the linked list.
+// EXAMPLE
+// Input: A -> B -> C - > D -> E -> C [the same C as earlier)
+// Output: C
+
+public LinkedListNode loopDetection(LinkedListNode node) {
+	if (node == null || node.next == null) return null;
+	LinkedListNode fast = node, slow = node;
+	while (fast != null && slow != null) {
+		fast = fast.next.next;
+		slow = slow.next;
+		if (fast == slow)break;
+	}
+	if (fast == null || slow == null) return null;
+	slow = node;
+	while (slow != fast) {
+		slow = slow.next;
+		fast = fast.next;
+	}
+	return fast;
+}
